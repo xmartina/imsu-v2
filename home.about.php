ee30e1a902395d25715ef2cade1fef3e21140e9f
unknown <martinsprince126@gmail.com>
2023-01-27 06:54:15 +0100

    s

--- home.about.php
+++ home.about.php
@@ -34,18 +34,66 @@
     <style>
         .vc-img{
             width: 100%;
-            height: 35em;
+            height: 37em;
             background-image: url('../imsu-v2/images/cus-img/vc-1.jpg');
             background-size: cover;
             background-position: center top;
-
+        }
+        .vc-img-overlay{
+            width: 100%;
+            height: 30%;
+            bottom: 0;
+            background-color: #0e0e0e;
+            padding: 2.5em;
+        }
+        .p_relative{
+            position: relative;
+        }
+        .p_absolute{
+            position: absolute;
+        }
+        .vc-img-overlay-head{
+            color: #f0f0f0;
+            font-size: 1.2em;
+        }
+        .vc-img-overlay-content{
+            margin-top: .6em;
+        }
+        .vc-content-1{
+            font-size: 1em;
+            color: #aeaeb0;
+        }
+        .vc-content-2{
+            font-size: 1.4em;
+            color: #8c8c94;
+        }
+        .vc-content-3{
+            font-size: 1.8em;
+            color: #ffffff;
         }
     </style>
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
                 <div class="vc-block">
-                    <div class="vc-img"></div>
+                    <div class="vc-img p_relative">
+                        <div class="vc-img-overlay p_absolute">
+                            <div class="vc-img-overlay-head">
+                                Prof Uchefula Ugonna Chukwumaeze
+                            </div>
+                            <div class="vc-img-overlay-content">
+                                <div class="vc-content-1">
+                                    Ph.D San
+                                </div>
+                                <div class="vc-content-2">
+                                    Vice-Chancellor
+                                </div>
+                                <div class="vc-content-2">
+                                    Imo State University
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-8">
