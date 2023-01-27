f710390806d8dc21d48fd424e16405535cf9a50d
unknown <martinsprince126@gmail.com>
2023-01-27 12:07:51 +0100

    s

--- home.about.php
+++ home.about.php
@@ -104,7 +104,7 @@
             background-color: #0a2444;
             color: #f0f0f0;
             font-size: 1.1em;
-            width: 13em;
+            width: 9em;
             height: 3em;
             padding-left: .8em;
             padding-top: .3em;
